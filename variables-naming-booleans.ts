8c9201da165bdcb9f710c17594efc34996cdc322
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-25 15:38:57 -0300

    naming booleans variables best practices

---
+++ variables-naming-booleans.ts
@@ -0,0 +1,19 @@
+// Wrong
+const userMajority = true
+
+// Correct 
+const isUserMajority = true
+
+/**
+ * When working with booleans, the name must be like a question, like
+ * isEmpty
+ * hasPermission
+ * doesContainPropertyX
+ */
+
+// Wrong
+const disabled = true
+
+// Correct
+const isDisabled = true
+const userHasGroup = true
