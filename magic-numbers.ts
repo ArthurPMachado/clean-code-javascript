eff632d09111e6040a947d3a518d6f91e9531259
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-25 17:34:31 -0300

    magic numbers

---
+++ magic-numbers.ts
@@ -0,0 +1,15 @@
+// Simple
+
+// setTimeout, setInterval
+
+const INTERVAL_30_DAYS = 1000 * 60 * 60 * 24 * 30
+
+setTimeout(() => {
+
+}, INTERVAL_30_DAYS)
+
+// Advanced
+
+function calculateDiscount(priceInCents, discountAmountInPercentage) {
+  // return discount
+}
