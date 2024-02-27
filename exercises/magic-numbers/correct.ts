53272315839aafc011b64c5441cc1726b226125d
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-26 22:01:52 -0300

    correct code to avoid magic numbers

---
+++ exercises/magic-numbers/correct.ts
@@ -0,0 +1,7 @@
+function lookForUpdades() {}
+
+const INTERVAL_30_MINUTES = 1000 * 60 * 30
+
+setInterval(lookForUpdades, INTERVAL_30_MINUTES)
+
+function calculateDiscount(priceInCents, discountAmountInPercentage) {}
