1c92b0ef92742ff80aaa72076a7b87761ed4546a
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-26 21:59:44 -0300

    correct code to destructure function params

---
+++ exercises/parameters-and-destructuring/correct.ts
@@ -0,0 +1,25 @@
+function updateUserRoute(body, params) {
+  const { name, email, password } = body
+  const { id } = params
+
+  updateUserController({
+    name,
+    email,
+    password
+  }, { id })
+}
+
+function updateUserController(data, params) {
+  const { name, email, password } = data
+  const { id } = params
+
+  userRepository.update({
+    name,
+    email,
+    password
+  }, { id })
+}
+
+const userRepository = {
+  update: (data, params) => {},
+}
